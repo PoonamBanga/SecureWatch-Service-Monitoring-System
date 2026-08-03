779f62fad61f2edb9c7a7eee8c6c076eb9d3f5ee
Poonam Banga <poonam2247rintern@gmail.com>
2026-08-03 14:16:59 +0530

    Containerize full stack with Docker Compose (web, worker, beat, redis, postgres)

--- config/settings.py
+++ config/settings.py
@@ -139,6 +139,14 @@ REST_FRAMEWORK ={
     'DEFAULT_AUTHENTICATION_CLASSES' :(
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_THROTTLE_CLASSES':[
+        'rest_framework.throttling.UserRateThrottle',
+        'rest_framework.throttling.AnonRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '100/minute',
+        'anon': '10/minute',
+    },
 }
 
 SIMPLE_JWT ={
@@ -148,8 +156,8 @@ SIMPLE_JWT ={
     'BLACKLIST_AFTER_ROTATION': True,
     }
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND ='redis://localhost:6379/0'
+CELERY_BROKER_URL = config('CELERY_BROKER_URL', default='redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = config('CELERY_RESULT_BACKEND', default='redis://localhost:6379/0')
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER ='json'
